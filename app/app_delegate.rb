687e9ed77667a2bbc40d6b41ac495f2d9e4f79f8
Jan Weinkauff <jan.weinkauff@me.com>
2012-05-24 23:42:47 +0200

    added base setup to app delegate for testing

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,8 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    MotionRecord::Manager.entity_classes = [ Project, Task ]
+    MotionRecord::Manager.shared
+    
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
     true
