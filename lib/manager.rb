0e6b1576ac549d78281e5fd6a35db94c234b34bb
Jan Weinkauff <jan.weinkauff@me.com>
2012-05-24 23:45:15 +0200

    refactored core data stack creation to make it possible to destroy a store

--- lib/manager.rb
+++ lib/manager.rb
@@ -1,7 +1,7 @@
 module MotionRecord
   
   class Manager
-    attr_accessor :model, :store, :context
+    attr_reader :model, :coordinator, :store, :context
   
   public
     def self.shared
@@ -9,7 +9,7 @@ module MotionRecord
     end
 
     def self.entity_classes
-      @@entity_classes
+      @@entity_classes ||= [ ]
     end
   
     def self.entity_classes=(new_entity_classes)
@@ -17,13 +17,13 @@ module MotionRecord
     end
     
     def self.entity_for_name(name)
-      NSEntityDescription.entityForName(name, inManagedObjectContext:shared.context)
+      NSEntityDescription.entityForName(name.to_s, inManagedObjectContext:shared.context)
     end
   
     def self.new_object_for_name(name)
-      NSEntityDescription.insertNewObjectForEntityForName(name, inManagedObjectContext:shared.context)
+      NSEntityDescription.insertNewObjectForEntityForName(name.to_s, inManagedObjectContext:shared.context)
     end
-  
+    
     def save
       error = Pointer.new(:object)
       unless @context.save(error)
@@ -32,69 +32,72 @@ module MotionRecord
       end
       true
     end
+    
+    def destroy
+      error = Pointer.new(:object)
+      @coordinator.removePersistentStore(@store, error:error)
+      unless error
+        puts "destroy: #{error[0].description}"
+      end
+      @store = nil  
+    end
   
     def execute_fetch_request(request, error:error)
       @context.executeFetchRequest(request, error:error)
     end
       
-  private
-    def initialize 
-      setup_model
-      setup_store
-      setup_context
+    def model
+      @model ||= begin
+        model = NSManagedObjectModel.alloc.init
+        model.entities = Manager.entity_classes.map { |klass| klass.entity }
+        model        
+      end
     end
   
-    def setup_model
-      model = NSManagedObjectModel.alloc.init
-      model.entities = Manager.entity_classes.map { |klass| klass.entity }
-      @model = model
+    def coordinator
+      @coordinator ||= begin
+        NSPersistentStoreCoordinator.alloc.initWithManagedObjectModel(model)
+      end
     end
   
-    def create_entities
-      subclasses.map { |subclass| subclass.entity }
+    def store
+      @store ||= begin
+        url = create_url_for_environment
+        error = Pointer.new(:object)
+        store = coordinator.addPersistentStoreWithType(store_type_for_environment, configuration:nil, URL:url, options:nil, error:error)
+        unless store
+          raise "Can't add persistent SQLite store: #{error[0].description}"
+        end
+        store        
+      end
     end
     
-    def subclasses
-      # RubyMotion dosen't support ObjectSpace.each_object at the moment :-/
-      return []
-      
-      class_hash = {}
-      ObjectSpace.each_object do |obj|
-        if NSManagedObject == obj.class
-          if obj.ancestors.include? self
-            class_hash[obj] = true
-          end
-        end
+    def context
+      @context ||= begin
+        context = NSManagedObjectContext.alloc.init
+        context.persistentStoreCoordinator = coordinator
+        context
       end
-      class_hash.keys
     end
-  
-    def setup_store
-      store = NSPersistentStoreCoordinator.alloc.initWithManagedObjectModel(@model)
-      url = create_url_for_environment
-      error = Pointer.new(:object)
-      unless store.addPersistentStoreWithType(store_type_for_environment, configuration:nil, URL:url, options:nil, error:error)
-        raise "Can't add persistent SQLite store: #{error[0].description}"
-      end
-      @store = store
+      
+  private
+    def initialize 
+      model
+      coordinator
+      store
+      context
     end
   
     def create_url_for_environment
       # we a way to know which environment is used (e.g. running specs in simulator)
-      filename = 'development.sqlite'
+      filename = $running_specs ? 'specs.sqlite' : 'development.sqlite'
       NSURL.fileURLWithPath(File.join(NSHomeDirectory(), 'Documents', filename)) 
     end
   
     def store_type_for_environment
-      # we a way to know which environment is used (e.g. running specs in simulator)
-      NSSQLiteStoreType
+      $running_specs ? NSInMemoryStoreType : NSSQLiteStoreType
     end
-  
-    def setup_context
-      context = NSManagedObjectContext.alloc.init
-      context.persistentStoreCoordinator = @store
-      @context = context
-    end
+    
   end
   
 end
